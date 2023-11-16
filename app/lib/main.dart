3884ff06f79fb6cdd562588500f9eb7a4f248a47
Ailton Venancio <ailton.venancio@cmsw.com>
2023-11-16 11:30:20 -0300

    feat: setup firebase analytics

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,27 +1,21 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:app/firebase_options.dart';
 
+class FirebaseService {
+  static final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalytics get instanceAnalytics => _analytics;
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(const MyApp());
-
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
-
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
 }
 
 class MyApp extends StatelessWidget {
@@ -59,6 +53,21 @@ class PageHome extends StatelessWidget {
                   child: OutlinedButton(
                     onPressed: () => throw Exception(),
                     child: const Text('Throw Test Exception'),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () async {
+                      await FirebaseService.instanceAnalytics.logEvent(
+                        name: "teste_analytics",
+                        parameters: {
+                          "status_teste": "ok",
+                          'timestamp': DateTime.now().toString(),
+                        },
+                      );
+                    },
+                    child: const Text('Sends data to Analytics'),
                   ),
                 ),
               ],
