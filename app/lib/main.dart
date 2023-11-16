92bf01b1570cd932caaaf62e49edd57b2a1e6f6d
Ailton Venancio <ailton.venancio@cmsw.com>
2023-11-16 16:27:17 -0300

    fix: firebase crashlytics main

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:app/firebase_options.dart';
@@ -16,6 +18,15 @@ void main() async {
   );
 
   runApp(const MyApp());
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 }
 
 class MyApp extends StatelessWidget {
